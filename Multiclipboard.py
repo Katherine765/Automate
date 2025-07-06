7dcc3792662899bef6bf5c4efc75f5ed822ffe68
kberger <superkitkat102@gmail.com>
2025-07-06 18:49:14 -0400

    removed typing what to save

--- Multiclipboard.py
+++ Multiclipboard.py
@@ -1,4 +1,4 @@
-# could add more features like deleting things and somehow allowing strings that you enter to have spaces and in general make this more user friendly... although could also get rid of the adding your own words option bc what is the point this isn't a notes app
+# could add more features like deleting things
 
 import shelve
 import pyperclip
@@ -7,8 +7,6 @@ import sys
 shelf = shelve.open('multiclipboard')
 
 # sys.argv[0] is always the script name (Multiclipboard.py)
-if len(sys.argv) == 4 and sys.argv[1] == 'save':
-    shelf[sys.argv[2]] = sys.argv[3]
 if len(sys.argv) == 3 and sys.argv[1] == 'save':
     shelf[sys.argv[2]] = pyperclip.paste()
 elif len(sys.argv) == 2:
